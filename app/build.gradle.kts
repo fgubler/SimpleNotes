22362792b9e919708006cf5ff99cd4ead5edd1b9
Florian Gubler <florian.gubler@gmail.com>
2024-10-25 17:58:04 +0200

    New version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ch.abwesend.simplenotes"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.3.0"
+        versionCode = 6
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
